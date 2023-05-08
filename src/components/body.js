5465949a8e86086df2a8569d34b570423539b02f
Narendra Singh Suyal <suyalnarendra7@gmail.com>
2023-05-08 12:37:35 +0530

    used swiggy api to get the data

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { restList } from "../config";
 import Card from "./restCard";
+import Shimmer from "./shimmer";
 
 
-const filterRest = (restList,nam)=> {
-    const filtdata = restList.filter((rst)=>
+const filterRest = (rest,nam)=> {
+    const filtdata = rest.filter((rst)=>
     {
         
-         return   rst.data.data.name.toLowerCase().includes(nam.toLowerCase());
+         return   rst?.data?.data?.name?.toLowerCase().includes(nam.toLowerCase());
     }
  );
     return filtdata;
@@ -15,8 +16,26 @@ const filterRest = (restList,nam)=> {
 
 const Body = ()=>{
     const [name,setName] = useState("");
-    const [rest,setRest] = useState(restList);
-    return (
+    const [rest,setRest] = useState([]);
+    const [finalRest,setFinalRest] =useState("");
+
+    useEffect(()=>{
+        getRests();
+},[]);
+
+  async function getRests() {
+    const data =await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9875082&lng=79.4141214&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING");
+
+    const json =await data.json();
+    console.log(json);
+    setRest(json?.data?.cards);
+    setFinalRest(json?.data?.cards);
+  }
+
+    
+    return rest.length === 0 ?(
+        <Shimmer/>)
+        :(
     <>
     <div className="searchBox">
         <input
@@ -29,7 +48,7 @@ const Body = ()=>{
         />
        <button
        onClick={()=>{
-        const data = filterRest(restList,name);       
+        const data = filterRest(finalRest,name);       
         setRest(data);
        }}
        >search</button>
---
+++ src/components/shimmer.js
@@ -0,0 +1,9 @@
+const Shimmer = ()=>{
+return (
+    <h1> Wait a Sec ....
+               ......LoaDing...
+    </h1>
+)
+}
+
+export default Shimmer;
